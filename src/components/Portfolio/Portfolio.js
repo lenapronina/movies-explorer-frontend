6277de4d19037a76f9605f135534b9c77ec85983
lenapronina <kaknetak@gmail.com>
2021-02-24 22:17:45 +0300

    feat: add portfolio component, update student block

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,28 @@
 import './Portfolio.css';
+import linkArrow from '../../images/portfolio-arrow.svg';
 
 function Portfolio() {
   return(
-    <div>haha</div>
+    <div className="portfolio aboutme__portfolio">
+      <h3 className="portfolio__heading">Портфолио</h3>
+      <ul className="portfolio__works">
+        <li className="portfolio__works-item">
+          <a className="portfolio__link" href="ya.ru">Статичный сайт
+            <img className="portfolio__link-arrow" alt="alt" src={linkArrow} />
+          </a>
+        </li>
+        <li className="portfolio__works-item">
+          <a className="portfolio__link" href="ya.ru">Адаптивный сайт
+            <img className="portfolio__link-arrow" alt="alt" src={linkArrow} />
+          </a>
+        </li>
+        <li className="portfolio__works-item">
+          <a className="portfolio__link" href="http://ya.ru">Одностраничное приложение
+            <img className="portfolio__link-arrow" alt="alt" src={linkArrow} />
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
