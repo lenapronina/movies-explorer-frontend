d2e903129c7ba05c2ef58d6ab596436fd7be9a14
lenapronina <kaknetak@gmail.com>
2021-02-27 23:59:37 +0300

    feat: add profile page

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,10 +3,6 @@ import accountIcon from '../../images/navigation-account-icon.svg';
 
 function Button({clss, text}) {
 
-// <button className="account-button">Аккаунт
-//         <img alt="Иконка" src={accountIcon}/>
-//       </button> 
-
   const checkBtn = ( clss === 'button_type_account' ) ? ( <img className="button__image" alt="Иконка" src={accountIcon}/> ) : null
   return(
     <button className={`button ${clss}`}>
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Button from '../Button/Button';
 import './Profile.css';
 
 function Profile(params) {
+
+  const onChange = () =>{
+    console.log('меняем')
+  }
+
   return(
-    <div className="profile">ХООХООХХХО</div>
+    <div className="profile">
+      <h2 className="profile__heading">Привет, Виталий!</h2>
+      <form className="profile__form">
+        <label className="profile__label">Имя
+          <input className="profile__input" name="user" type="text" minLength="2" maxLength="40" required="" value="Виталя" onChange={onChange} disabled></input>
+        </label>
+        <label className="profile__label">Почта
+          <input className="profile__input" name="mail" type="mail" value="pochta@yandex.ru" onChange={onChange} disabled></input>
+        </label>
+      </form>
+      <div className="profile__controllers">
+        <Button 
+          clss='button_type_profile-edit' 
+          text='Редактировать'
+        />
+        <Button 
+          clss='button_type_profile-signout' 
+          text='Выйти из аккаунта'
+        />
+      </div>
+    </div>
   )
 }
 
