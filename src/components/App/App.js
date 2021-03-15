ec5455386d81270cfddd6bb0814ff8a437216118
lenapronina <kaknetak@gmail.com>
2021-03-15 10:35:16 +0300

    feat: add onRegister func

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,14 @@ import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
 
+import * as MainApi from '../../utils/MainApi';
+
+import { CurrentUserContext } from '../../contexts/UserContext';
+
 function App() {
+
+  // set useState for CurrentUserContext
+  const [currentUser, setCurrentUser] = useState('');
 
   const history = useHistory();
 
@@ -24,6 +31,8 @@ function App() {
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
   const [path, setPath] = useState('/');
+
+  const [registerSuccess, setRegisterSuccess] = useState(false);
 
   const routes = ["/signup", "/signin", "/notFound"];
 
@@ -88,6 +97,26 @@ function App() {
     setLoggedIn(false);
   }
 
+  function handleRegisterSuccess(value){
+    setRegisterSuccess(value);
+  }
+
+
+  const onRegister = (name, email, password) => {
+    MainApi.register(name, email, password)
+    .then((res) => {
+      if(res.statusCode !== 400){
+        handleRegisterSuccess(true);
+      } else {
+        handleRegisterSuccess(false);
+      }
+    })
+    .catch((err)=> {
+      handleRegisterSuccess(false);
+      console.log(err);
+    });
+  }
+
   const checkLoggedIn = useCallback(() => {
     if(history.location.pathname === '/'){
       setLoggedIn(true);
@@ -105,78 +134,81 @@ function App() {
   }, [checkLoggedIn])
 
   return (
-    <div className="app">
-      { useRouteMatch(routes) ?
-        null
-        : ( 
-            <>
-              <Header 
-                pathName={path}
-                loggedIn={loggedIn}
-                signIn={goSignIn}
-                signUp={goSignUp}
-                goMovies={goMovies}
-                goSavedMovies={goSavedMovies}
-                goProfile={goProfile}
-                goLanding={goLanding}
-                mobileMenu={mobileMenu}
-                clickMenu={toggleMobileMenu}
-              />
-              <AsideMenu
-                isOpen={isMenuOpen}
-                closeMenu={toggleMobileMenu} 
-                signIn={goSignIn}
-                signUp={goSignUp}
-                pathName={path}
-                goMovies={goMovies}
-                goProfile={goProfile}
-                goSavedMovies={goSavedMovies}
-                mobileMenu={mobileMenu}
-              />
-            </>
-          )
-      }
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
-        <Route path="/signup">
-          <Register 
-            goLanding={goLanding} 
-            signIn={goSignIn}
-          />
-        </Route>
-        <Route path="/signin">
-          <Login
-            goLanding={goLanding} 
-            signUp={goSignUp}
-          />
-        </Route>
-        <Route path="/profile">
-          <Profile 
-            goLanding={goLanding}
-          />
-        </Route>
-        <Route path="/movies">
-          <Movies 
-            path='/movies'
-          />
-        </Route>
-        <Route path="/saved-movies">
-          <SavedMovies 
-            path='/saved-movies'
-          />
-        </Route>
-        {/* Redirect would be fix to real path in the next step */}
-        <Route path="/notFound">
-          <NotFoundPage 
-            goBack={goBack}
-          />
-        </Route>
-        <Redirect from="*" to="/notFound" />
-         {/* Redirect would be fix to real path in the next step */}
-      </Switch>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="app">
+        { useRouteMatch(routes) ?
+          null
+          : ( 
+              <>
+                <Header 
+                  pathName={path}
+                  loggedIn={loggedIn}
+                  signIn={goSignIn}
+                  signUp={goSignUp}
+                  goMovies={goMovies}
+                  goSavedMovies={goSavedMovies}
+                  goProfile={goProfile}
+                  goLanding={goLanding}
+                  mobileMenu={mobileMenu}
+                  clickMenu={toggleMobileMenu}
+                />
+                <AsideMenu
+                  isOpen={isMenuOpen}
+                  closeMenu={toggleMobileMenu} 
+                  signIn={goSignIn}
+                  signUp={goSignUp}
+                  pathName={path}
+                  goMovies={goMovies}
+                  goProfile={goProfile}
+                  goSavedMovies={goSavedMovies}
+                  mobileMenu={mobileMenu}
+                />
+              </>
+            )
+        }
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/signup">
+            <Register 
+              goLanding={goLanding} 
+              signIn={goSignIn}
+              handleRegister={onRegister}
+            />
+          </Route>
+          <Route path="/signin">
+            <Login
+              goLanding={goLanding} 
+              signUp={goSignUp}
+            />
+          </Route>
+          <Route path="/profile">
+            <Profile 
+              goLanding={goLanding}
+            />
+          </Route>
+          <Route path="/movies">
+            <Movies 
+              path='/movies'
+            />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies 
+              path='/saved-movies'
+            />
+          </Route>
+          {/* Redirect would be fix to real path in the next step */}
+          <Route path="/notFound">
+            <NotFoundPage 
+              goBack={goBack}
+            />
+          </Route>
+          <Redirect from="*" to="/notFound" />
+           {/* Redirect would be fix to real path in the next step */}
+        </Switch>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,33 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Register.css';
 import SignHeader from '../SignHeader/SignHeader';
 import Button from '../Button/Button';
 
-function Register({goLanding, signIn}){
+function Register({goLanding, signIn, handleRegister}){
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    console.log('21212')
+    e.preventDefault();
+    handleRegister(name, email, password);
+  }
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  }
+
   return(
     <div className="sign-container">
       <SignHeader 
         handleLogo={goLanding} 
         headerText="Добро пожаловать!"
       />
-      <form className="sign-form">
+      <form 
+        className="sign-form"
+        onSubmit={handleSubmit}
+      >
         <label htmlFor="user-name" className="sign-label">Имя
-          <input id="user-name" className="sign-input" type="text" placeholder="Виталий" autoComplete="on" required></input>
+          <input 
+            id="user-name"
+            className="sign-input"
+            type="text"
+            placeholder="Виталий"
+            autoComplete="on"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
         </label>
         <label htmlFor="user-mail" className="sign-label">E-mail
-          <input id="user-mail" className="sign-input" type="email" placeholder="pochta@yandex.ru" autoComplete="on" required></input>
+          <input
+            id="user-mail"
+            className="sign-input"
+            type="email"
+            placeholder="pochta@yandex.ru"
+            autoComplete="on"
+            value={email}
+            onChange={handleEmailChange}
+            required 
+          />
         </label>
         <label htmlFor="user-password" className="sign-label sign-label__error">Пароль
-          <input id="user-password" className="sign-input sign-input__error" type="password" autoComplete="on" required></input>
+          <input 
+            id="user-password"
+            className="sign-input"
+            type="password"
+            autoComplete="on"
+            value={password}
+            onChange={handlePasswordChange}
+            required 
+          />
         </label>
         <span className="sign__error">Что-то пошло не так...</span>
-      </form>
-      <div className="sign-confirm sign-form__sign-register">
         <Button 
           mode="button_type_sign"
           type="submit" 
           text="Зарегестрироваться"
         />
+      </form>
+      <div className="sign-confirm sign-form__sign-register">
+        
         <span className="sign__paragraph">Уже зарегистрированы?&nbsp;
           <Button
             mode="button_type_sign-simple"
